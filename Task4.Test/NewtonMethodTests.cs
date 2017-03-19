df633190c8ee86ac0b46f3e011b4ef437569fbc2
AlexKarmaz <karmazct@mail.ru>
2017-03-20 00:04:27 +0300

    Add Task4

---
+++ Task4.Test/NewtonMethodTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Task4.Test
+{
+    [TestFixture]
+    public class NewtonMethodTests
+    {
+        [TestCase(0.015625, 3, 0.0000001, ExpectedResult = 0.25)]
+        [TestCase(9, 2, 0.00001, ExpectedResult = 3)]
+        [TestCase(438976, 3, 0.00000000000001, ExpectedResult = 76)]
+        [Test]
+        public double Sqrt_PositiveTest(double number, int n, double precision = 0.000001)
+        {
+            return NewtonMethod.Sqrt(number, n, precision);
+        }
+
+        [TestCase(-444, 4, 0.000001)]
+        [TestCase(0.0000002, 2, 0.00001)]
+        [Test]
+        public void Sqrt_ThrowsArgumentException(double number, int n, double precision)
+        {
+            Assert.Throws<ArgumentException>(() => NewtonMethod.Sqrt(number, n, precision));
+        }
+
+        [TestCase(-5432, 4, 2)]
+        [TestCase(-5432, 4, 1e-16)]
+        [TestCase(-5432, -3, 0.000001)]
+        [Test]
+        public void Sqrt_ThrowsArgumentOutOfRangeException(double number, int n, double precision)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => NewtonMethod.Sqrt(number, n, precision));
+        }
+    }
+}
---
+++ Task4/NewtonMethod.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    /// <summary>
+    /// Contains the method of searching the root by Newton's method
+    /// </summary>
+    public class NewtonMethod
+    {
+        /// <summary>Finding root of specified degree of the number</summary>
+        /// <param name="number">Number for which you want to search the root</param>
+        /// <param name="n">Degree of root</param>
+        /// <param name="precision">Precision of root</param>
+        /// <returns>Root of <paramref name="n"/>-degree for the <paramref name="number"/></returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        /// <exception cref="ArgumentException"></exception>
+        public static double Sqrt(double number, int n, double precision = 0.000001)
+        {
+            if (precision < 1e-15 || precision > 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(precision));
+            }
+
+            if (n < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(n));
+            }
+
+            if (number < 0 && n % 2 == 0)
+            {
+                throw new ArgumentException();
+            }
+
+            if (Math.Abs(number) < precision)
+            {
+                throw new ArgumentException();
+            }
+
+
+            double x0, x1 = number;
+
+            do
+            {
+                x0 = x1;
+                x1 = x0 - (Math.Pow(x0, n) - number) / (Math.Pow(x0, n - 1) * n);
+            } while (Math.Abs(x1 - x0) > precision);
+
+
+            return x1;
+        }
+    }
+}
